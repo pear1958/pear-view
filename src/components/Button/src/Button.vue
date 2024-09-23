0616174e42bedeb5378a82564a9ae2ee1e92a86e
zhangzy <doxoops@163.com>
2024-09-23 08:24:51 +0800

    fix: css

--- src/components/Button/src/Button.vue
+++ src/components/Button/src/Button.vue
@@ -9,13 +9,3 @@ defineOptions({
   name: 'Button'
 })
 </script>
-
-<style lang="scss" scoped>
-.btn {
-  padding: 8px 12px;
-  border: 1px solid var(--pear-primary);
-  border-radius: 3px;
-  display: inline-block;
-  cursor: pointer;
-}
-</style>
