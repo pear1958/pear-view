ee2d97393cf2366da4aeb4426efadfb26087cf97
zhangzy <doxoops@163.com>
2024-09-28 12:30:57 +0800

    fix: fileName

--- packages/components/src/Button/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import Button from './src/button.vue'
-
-Button.install = (app: App) => {
-  app.component(Button.name as string, Button)
-}
-
-export { Button }
-export default Button
