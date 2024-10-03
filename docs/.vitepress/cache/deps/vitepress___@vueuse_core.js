3f57799edad2d394e5cde0db4aa94ea78b95ca75
zhangzy <doxoops@163.com>
2024-10-04 03:50:01 +0800

    fix: version

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -35,7 +35,7 @@ import {
   watchEffect
 } from "./chunk-IW3GZR3P.js";
 
-// ../node_modules/.pnpm/vitepress@1.2.3_@algolia+client-search@5.7.0_@types+node@22.7.0_postcss@8.4.47_sass@1.78.0_se_o6ot2cp46xwgetqzeqgmhej62a/node_modules/vitepress/lib/vue-demi.mjs
+// ../node_modules/.pnpm/vitepress@1.2.3_@algolia+client-search@5.7.0_search-insights@2.17.2/node_modules/vitepress/lib/vue-demi.mjs
 var isVue2 = false;
 var isVue3 = true;
 function set(target, key, val) {
