97091cbf02c1c2ef97c84d73ac95e94f66cb0844
zhangzy <zhangzhiyong@klicen.com>
2024-10-11 16:59:43 +0800

    fix: 精简代码

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,6 +1,1 @@
-export * from './src/uuid'
-export * from './src/theme'
-export * from './src/is'
-export * from './src/index'
-
-export { default as Bus } from './src/bus'
+export * from './src'
--- packages/utils/publish/index.ts
+++
@@ -1,9 +0,0 @@
-import { series } from 'gulp'
-
-import run from '../../components/script/utils/run'
-
-export const publishUtils = async () => {
-  run('release-it', '../dist')
-}
-
-export default series(async () => publishUtils())
